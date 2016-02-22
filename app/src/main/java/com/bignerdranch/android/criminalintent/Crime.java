251332568de328bdd6e681374f158865cffdf18f
Ben Medcalf <ben.medcalf@snagajob.com>
2016-02-22 08:58:34 -0500

    finishing chapter 8

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
 import java.util.UUID;
+import java.util.Date;
 
 /**
  * Created by Ben on 2/10/16.
@@ -9,10 +10,13 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
         //Generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
@@ -26,4 +30,13 @@ public class Crime {
     public void setTitle(String title) {
         mTitle = title;
     }
+
+    public Date getDate() {return mDate;}
+
+    public boolean isSolved() {return mSolved;}
+
+    public void setDate(Date mDate) {this.mDate = mDate;}
+
+
+    public void setSolved(boolean mSolved) {this.mSolved = mSolved;}
 }
